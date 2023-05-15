85f430d846c3191e488262abb37e5d8ab01fef7d
Kirill Golygin <kirik3007@gmail.com>
2023-05-15 16:17:55 +0300

    dynamic-video-player

--- src/js/main.js
+++ src/js/main.js
@@ -7,8 +7,8 @@ window.addEventListener('DOMContentLoaded', () => {
         });
     slider.render();
 
-    const player = new VideoPlayer('.showup .play', '.overlay');
-    player.init();
+    new VideoPlayer('.showup .play', '.overlay').init();
+    new VideoPlayer('.module__video-item .play', '.overlay').init();
 
     const showUpSlider = new MiniSlider({
         container: '.showup__content-slider',
